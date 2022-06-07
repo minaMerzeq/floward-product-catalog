b11b72b548bec8b208e54992d52eb623a0b2ddab
Mina Merzeq <minamerzekluffy@gmail.com>
2022-06-07 09:57:39 +0200

    product service initial architecture

---
+++ Floward.Product.Catalog.Service/Floward.Product.Catalog.Service/Domain/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Floward.Product.Catalog.Service.Domain.Entities
+{
+    public class Product
+    {
+    }
+}
